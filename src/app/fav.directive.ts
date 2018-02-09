c44dd8742a35131dbb3e914690a4d16bba643d63
manojvarmagottumukkala@gmail.com <manojvarmagottumukkala@gmail.com>
2018-02-09 17:05:30 -0500

    "Custome Directive"

--- src/app/fav.directive.ts
+++ src/app/fav.directive.ts
@@ -1,13 +1,38 @@
-import { Directive } from '@angular/core';
+import { Directive,HostBinding,HostListener,Input } from '@angular/core';
+import { leave } from '@angular/core/src/profile/wtf_impl';
+
 
 @Directive(
 {
-selector:"Fav"
+selector:"[Fav]"
 
 
 })
 
 export class FavDirective
 {
+
+@HostBinding('class.isFavorite') Favorite=true;
+
+@Input() set Fav(value)
+{
+this.Favorite=value;
+
+}
+
+@HostBinding('class.isHovering') Hovering;
+
+
+@HostListener('mouseenter')Enter()
+{
+this.Hovering=true;
+}
+
+@HostListener('mouseleave')leave()
+{
+    this.Hovering=false;
+}
+
+
 
 }
